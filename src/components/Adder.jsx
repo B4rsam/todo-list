a8ca2186ff1bccc0fa1c50967943d494864a0221
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-12-09 17:37:28 +0330

    feat: added wrapper

--- src/components/Adder.jsx
+++ src/components/Adder.jsx
@@ -14,17 +14,19 @@ const AddTask = ({onAdd, onExit}) => {
     }
 
     return (
-        <div className='container addBox'>
-            <div className='header addheader'>
-               <h1>Add a Task</h1> 
-               <button className='btn delbtn' onClick={onExit}>Cancel</button>
+        <div className='wrapper'>
+            <div className='container addBox'>
+                <div className='header addheader'>
+                    <h1>Add a Task</h1> 
+                    <button className='btn delbtn' onClick={onExit}>Cancel</button>
+                </div>
+                <form>
+                    <input id="taskData" className="taskDescription" placeholder='Task Description'></input>
+                    <input id="taskStatus" type='checkbox'></input>
+                    <span>Completed</span>
+                </form>
+                <button className='btn taskbtn' onClick={handleSubmit}>Submit Task</button>
             </div>
-            <form>
-                <input id="taskData" className="taskDescription" placeholder='Task Description'></input>
-                <input id="taskStatus" type='checkbox'></input>
-                <span>Completed</span>
-            </form>
-            <button className='btn taskbtn' onClick={handleSubmit}>Submit Task</button>
         </div>
     )
 }
