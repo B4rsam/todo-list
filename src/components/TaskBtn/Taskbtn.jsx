484fe68029e7588d60624acb777d6f0133c9c0c5
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-01-20 14:49:02 +0330

    feat: addTask now prevents scrollign

--- src/components/TaskBtn/Taskbtn.jsx
+++ src/components/TaskBtn/Taskbtn.jsx
@@ -8,6 +8,12 @@ const Taskbtn = ({onAdd}) => {
 
     const handleModal = () => {
         showModal(!modal);
+        if (document.body.style.overflow != "hidden") {
+            document.body.style.overflow = "hidden"    
+        } else {
+            document.body.style.overflow = "scroll"
+        }
+        
     }
 
     return (
